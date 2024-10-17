b004f4151324762b99761218749368fd7d7a5ffe
NickW16 <171855220+NickW16@users.noreply.github.com>
2024-10-17 16:35:45 -0300

    Create functions for operations

--- script.js
+++ script.js
@@ -2,25 +2,38 @@
 function add (a, b) {
     let result = a + b;
     return result
-}
+};
 
 function subtract (a, b) {
     let result = a - b;
     return result
-}
+};
 
 function multiply (a, b) {
     let result = a * b;
     return result
-}
+};
 
 function divide (a, b) {
     let result = a / b;
     return result
-}
+};
 
+/*
 let numbOne = 10;
 let numbTwo = 5;
 
 let result = subtract(numbOne, numbTwo);
 console.log(result);
+/*
+
+/* operation variables */
+
+let numbOne = '';
+let numbTwo = '';
+let operation = '';
+
+function operate (a, b, c) {
+    let result = a, b, c;
+    return result
+}
