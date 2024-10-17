802907ca6295106a418777c73683e079ae8a731a
NickW16 <171855220+NickW16@users.noreply.github.com>
2024-10-17 16:15:53 -0300

    Add mathematic functions

---
+++ script.js
@@ -0,0 +1,26 @@
+/* mathematic functions: */
+function add (a, b) {
+    let result = a + b;
+    return result
+}
+
+function subtract (a, b) {
+    let result = a - b;
+    return result
+}
+
+function multiply (a, b) {
+    let result = a * b;
+    return result
+}
+
+function divide (a, b) {
+    let result = a / b;
+    return result
+}
+
+let numbOne = 10;
+let numbTwo = 5;
+
+let result = subtract(numbOne, numbTwo);
+console.log(result);
