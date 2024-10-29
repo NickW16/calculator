906773dd4c6b67ae0aa7fa295f8e7d57e23990ca
NickW16 <nick@Xubuntu.myguest.virtualbox.org>
2024-10-29 00:48:24 -0300

    Add multiple  operator restrictions to prevent buggy operations

--- script.js
+++ script.js
@@ -54,15 +54,16 @@ document.querySelector("#plus-or-minus").addEventListener('click', function () {
     }
     isPositive = !isPositive; // toggle feature
 });
+
+let percentage = true;
+
 document.querySelector("#percentage").addEventListener('click', function () {
-    if (display.textContent.length < MAX_LENGTH) {
-        display.textContent += '%';
+    if (display.textContent.length < MAX_LENGTH && percentage) {
+        display.textContent = '0.' + display.textContent;
+    } else {
+        display.textContent = display.textContent.replace('0.','');
     }
-});
-document.querySelector("#divide").addEventListener('click', function () {
-    if (display.textContent.length < MAX_LENGTH) {
-        display.textContent += '/';
-    }
+    percentage = !percentage // toggle
 });
 document.querySelector("#numbSeven").addEventListener('click', function () {
     if (display.textContent.length < MAX_LENGTH) {
@@ -77,11 +78,6 @@ document.querySelector("#numbEight").addEventListener('click', function () {
 document.querySelector("#numbNine").addEventListener('click', function () {
     if (display.textContent.length < MAX_LENGTH) {
         display.textContent += '9';
-    }
-});
-document.querySelector("#multiply").addEventListener('click', function () {
-    if (display.textContent.length < MAX_LENGTH) {
-        display.textContent += '*';
     }
 });
 document.querySelector("#numbFour").addEventListener('click', function () {
@@ -119,19 +115,52 @@ document.querySelector("#numbZero").addEventListener('click', function () {
         display.textContent += '0';
     }
 });
+
+let maxOperator = 1;
+let operatorCount = 0; // this avoids equations with multiple operators
+
 document.querySelector("#add").addEventListener('click', function () {
-    if (display.textContent.length < MAX_LENGTH) {
+    if (display.textContent.length < MAX_LENGTH && operatorCount < maxOperator) {
+        operatorCount += 1;
         display.textContent += '+';
+    } else {
+        expressionResult(display.textContent)
     }
 });
 document.querySelector("#subtract").addEventListener('click', function () {
-    if (display.textContent.length < MAX_LENGTH) {
+    if (display.textContent.length < MAX_LENGTH && operatorCount < maxOperator) {
+        operatorCount += 1;
         display.textContent += '-';
+    } else {
+        expressionResult(display.textContent)
     }
 });
+document.querySelector("#multiply").addEventListener('click', function () {
+    if (display.textContent.length < MAX_LENGTH && operatorCount < maxOperator) {
+        operatorCount += 1;
+        display.textContent += '*';
+    } else {
+        expressionResult(display.textContent)
+    }
+});
+document.querySelector("#divide").addEventListener('click', function () {
+    if (display.textContent.length < MAX_LENGTH && operatorCount < maxOperator) {
+        operatorCount += 1;
+        display.textContent += '/';
+    } else {
+        expressionResult(display.textContent)
+    }
+});
+
+let maxDot = 1;
+let dotCount = 0; // this limits dots to 1.
+
 document.querySelector("#dot").addEventListener('click', function () {
-    if (display.textContent.length < MAX_LENGTH) {
+    if (display.textContent.length < MAX_LENGTH && dotCount < maxDot) {
+        dotCount += 1;
         display.textContent += '.';
+    } else {
+        expressionResult(display.textContent)
     }
 });
 
@@ -156,21 +185,31 @@ function operate (a, b, operation) {
 
 // this finishes the equation once '=' is pressed
 document.querySelector("#equal").addEventListener('click', function () {
-    const expression = display.textContent;
-    const parts = expression.split(/([\+\-\*\/])/);     // this part splits the string and turns it into an array
+    expressionResult(display.textContent)
+});
+    
+
+
+function expressionResult (a) {
+    const expression = a;
+    const parts = expression.split(/([\+\-\*\/])/).map(part => part.trim());     // this part splits the string and turns it into an array
 
     // this splits the array intro 3 parts: number 1, operator and number 2
     if (parts.length === 3 && parts[2] != '0') {
-        let numbOne = parseFloat(parts[0].trim());
-        let operation = parts[1].trim();
-        let numbTwo = parseFloat(parts[2].trim());
+        let numbOne = parseFloat(parts[0]);
+        let operation = parts[1];
+        let numbTwo = parseFloat(parts[2]);
+        if (isNaN(numbTwo)) {
+            numbTwo = numbOne;
+        };
 
         // calls the function
         let result = operate(numbOne, numbTwo, operation);
-
+        operatorCount = 0; // resets the operator
         // displays the result
         display.textContent = result;
+ 
     } else {
         display.textContent = 'Nice Try';
     }
-});
+};
